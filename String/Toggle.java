ad7ca625ad1ec7f566c979b8a9d18e3a44abbde0
Mani Bansal <44293399+ManiBansal0025@users.noreply.github.com>
2021-06-02 13:10:10 +0530

    Update Toggle.java

--- String/Toggle.java
+++ String/Toggle.java
@@ -2,22 +2,21 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-	    int count=0;
-	    Scanner sc = new Scanner(System.in);
-	    System.out.println("Enter the string");
-	    String s1 = sc.nextLine();
-      String s2 = "";
-	    for(int i=0;i<s1.length();i++){
-        char c = s1.charAt(i);
-        if(c==c.toUpperCase()){
-          c = c.toLowerCase();
-          s2=s2.append(c);
-	    }
-        else if (c==c.toLowerCase()){
-          c = c.toUpperCase();
-          s2=s2.append(c);
-        }
-        
-		System.out.println(s2);
+		System.out.println("Enter the string");
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		String s1 ="";
+		for(int i=0;i<s.length();i++){
+		    if(Character.isUpperCase(s.charAt(i)))
+		    {
+		        s1 = s1+Character.toLowerCase(s.charAt(i));
+		        
+		    }
+		    else{
+		        s1 = s1+Character.toUpperCase(s.charAt(i));
+		    }
+		}
+		System.out.println(s1);
 	}
 }
+
