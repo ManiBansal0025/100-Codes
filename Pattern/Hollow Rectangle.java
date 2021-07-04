ed8c4e109da7b03763a2ea2d4f78f9452b9df561
Mani Bansal <44293399+ManiBansal0025@users.noreply.github.com>
2021-07-04 08:21:02 +0530

    Create Hollow Rectangle.java

---
+++ Pattern/Hollow Rectangle.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Star Pattern");
+		for(int i=0;i<4;i++){
+		    for(int j=0;j<4;j++){
+		        if(i==0 || i==3||j==0||j==3){
+		           System.out.print("*");
+		        }
+		        else{
+		            System.out.print(" ");
+		        }
+		    }
+		    System.out.println();
+		}
+	}
+}
