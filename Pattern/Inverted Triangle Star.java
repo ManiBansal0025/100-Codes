987d101fc5d74226d663a64264166dc784c6c030
Mani Bansal <44293399+ManiBansal0025@users.noreply.github.com>
2021-07-04 08:11:07 +0530

    Create Inverted Triangle Star.java

---
+++ Pattern/Inverted Triangle Star.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Star Pattern");
+		for(int i=0;i<6;i++){
+		    for(int j=i+1;j<6;j++){
+		        System.out.print("*");
+		    }
+		    System.out.println(" ");
+		}
+	}
+}
