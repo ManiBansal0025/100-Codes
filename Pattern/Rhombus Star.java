032153eb4516c17c96db1821396e537177384c68
Mani Bansal <44293399+ManiBansal0025@users.noreply.github.com>
2021-07-04 08:36:17 +0530

    Create Rhombus Star.java

---
+++ Pattern/Rhombus Star.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Star Pattern");
+		for(int i=0;i<4;i++){
+		    for(int j=0;j<4;j++){
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		    for(int k=0;k<=i;k++){
+		        System.out.print(" ");   
+		    }
+		}
+	}
+}
