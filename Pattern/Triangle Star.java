0a3a9e0ebc803e7d5724bddb14d2c92a1b5f29da
Mani Bansal <44293399+ManiBansal0025@users.noreply.github.com>
2021-07-04 08:13:09 +0530

    Create Triangle Star.java

---
+++ Pattern/Triangle Star.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Star Pattern");
+		for(int i=0;i<6;i++){
+		    for(int j=0;j<=i;j++){
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	}
+}
