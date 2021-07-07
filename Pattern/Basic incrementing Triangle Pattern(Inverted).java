ef4cb5efe5afa0edfbdd9a4172045e93d6f5fa9f
Mani Bansal <44293399+ManiBansal0025@users.noreply.github.com>
2021-07-07 08:44:44 +0530

    Create Basic incrementing Triangle Pattern(Inverted).java

---
+++ Pattern/Basic incrementing Triangle Pattern(Inverted).java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int n = 3;
+		for(int i=0;i<4;i++){
+		    for(int j=0;j<=i;j++){
+		        System.out.print(n);
+		    }
+		    n++;
+		    System.out.println();
+		}
+	}
+}Basic incrementing Triangle Pattern(Inverted)
