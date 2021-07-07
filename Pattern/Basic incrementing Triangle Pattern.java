248c74b9225e653755356662ea3b18cdab212bbb
Mani Bansal <44293399+ManiBansal0025@users.noreply.github.com>
2021-07-07 08:41:06 +0530

    Create Basic incrementing Triangle Pattern.java

---
+++ Pattern/Basic incrementing Triangle Pattern.java
@@ -0,0 +1,14 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		int n = 6;
+		for(int i=0;i<4;i++){
+		    for(int j=i+1;j<=4;j++){
+		        System.out.print(n);
+		    }
+		    n--;
+		    System.out.println();
+		}
+	}
+}
